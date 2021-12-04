c1fc834f7e532089d8becb06dab07252b834ffdd
pooleya19 <34449819+pooleya19@users.noreply.github.com>
2021-12-04 03:29:32 -0500

    Added in basic visuals, updated Graph class, added MST maze generation

--- Graph.py
+++ Graph.py
@@ -1,49 +1,68 @@
 class Graph:
     def __init__(self):
-        self.adjList = {} # the dictionary that stores [node being pointed to, weight] at the key node that points
+        self.adjList = {} # the dictionary that stores [(node being pointed to, weight)] at the key node that points
         self.values = []  # the "map" that stores what values have already been added
 
 # used https://www.pythonpool.com/adjacency-list-python/ as reference
     def add_edge(self, node1, node2, weight):
         # first node is the node that points to the second node with value of weight
-        temp = []
 
-        if node1 not in self.values and node2 not in self.values:
-            # if neither node exists
-            self.adjList[node1] = None
-            self.adjList[node2] = None
+        if node1 not in self.values:
+            self.add_node(node1)
+        if node2 not in self.values:
+            self.add_node(node2)
 
-            self.values.append(node1)
-            self.values.append(node2)
-
-            temp.append([node2, weight])
-            self.adjList[node1] = temp
-
-        elif node2 not in self.values:
-            # if the first node exist but not the second
-            self.adjList[node2] = None
-            self.values.append(node2)
-
-            # in this case, the first node can already have pre-existing values so we account for that here
-            temp.extend(self.adjList[node1])
-            temp.append([node2, weight])
-            self.adjList[node1] = temp
-
-        elif node1 not in self.values:
-            # second node exists but not the first
-            self.adjList[node1] = None
-            self.values.append(node1)
-
-            temp.append([node2, weight])
-            self.adjList[node1] = temp
-
-        else:
-            temp.extend(self.adjList[node1])
-            temp.append([node2, weight])
-            self.adjList[node1] = temp
+        self.adjList[node1][node2] = weight
 
     def add_node(self, node): # this function may be useless but I'll add it anyways
         # making sure to use an if statement to prevent adding a node that exists
         if node not in self.values:
-            self.adjList[node] = None
+            self.adjList[node] = {}
             self.values.append(node)
+
+    def getEdgeCount(self):
+        edgeCount = 0
+        for node1 in self.adjList.keys():
+            edgeCount += len(self.adjList[node1].keys())
+        return edgeCount
+
+    def createMinimumSpanningTree(self):
+        #Setup lists
+        edges = []
+        MST = {}
+        remaining = {}
+        for node in self.adjList.keys():
+            remaining[node] = self.adjList[node]
+
+        #Add first node
+        firstKey = None
+        for key in remaining.keys():
+            firstKey = key
+            break
+        MST[firstKey] = remaining[firstKey]
+        remaining.pop(firstKey)
+
+        while(len(remaining) > 0):
+            minWeight = 2000000000 #init as large value -> find smallest
+            minEdge = (-1,-1)
+            toPop = []
+
+            for node1 in MST.keys():
+                stillUsable = False
+                for node2 in MST[node1].keys():
+                    if(remaining.get(node2) != None):
+                        stillUsable = True
+                        weight = MST[node1][node2]
+                        if(weight < minWeight):
+                            minWeight = weight
+                            minEdge = (node1,node2)
+                if(not stillUsable):
+                    toPop.append(node1)
+
+            for node in toPop:
+                MST.pop(node)
+
+            MST[minEdge[1]] = remaining[minEdge[1]]
+            remaining.pop(minEdge[1])
+            edges.append(minEdge)
+        return edges
---
+++ TestVisual.py
@@ -0,0 +1,124 @@
+"""
+INSTALL PACKAGES WITH PIP
+In command prompt:
+- Check python version: py --version
+- Install pip: py -m pip install --upgrade pip
+"""
+
+import pygame
+import sys
+from pygame.locals import *
+from Graph import Graph
+import time
+import random
+
+def main():
+    print("Beginning Program.")
+    screenSize = (1200,800)
+    global screen
+    screen = pygame.display.set_mode(screenSize)
+    print("Size:", screen.get_size())
+    pygame.display.set_caption("Maze Visual")
+
+    background = makeSurface(screen.get_size(), (180,180,180))
+    screen.blit(background, (0,0))
+
+    while True:
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                quit()
+            if event.type == KEYDOWN:
+                if(event.key == K_ESCAPE):
+                    quit()
+                elif(event.key == K_SPACE):
+                    generateMaze()
+
+def makeSurface(size, rgb):
+    surface = pygame.Surface(size)
+    surface = surface.convert()
+    surface.fill(rgb)
+    return surface
+
+def generateMaze():
+    startTime = time.time()
+    mazeBorderThick = 5
+    mazePos = (650, 150)
+    mazeSize = (500, 500)
+    mazeBorder = makeSurface((mazeSize[0] + 2 * mazeBorderThick, mazeSize[1] + 2 * mazeBorderThick), (150, 150, 150))
+    maze = makeSurface(mazeSize, (150, 150, 150))
+    screen.blit(mazeBorder, (mazePos[0] - mazeBorderThick, mazePos[1] - mazeBorderThick))
+    screen.blit(maze, mazePos)
+    pygame.display.update()
+
+    rows = 50
+    columns = 50
+
+    mazeBrushThickness = 5
+    if(rows > 50 or columns > 50): mazeBrushThickness = 2
+    mazeNodeSize = (mazeBrushThickness, mazeBrushThickness)
+    mazeNode = makeSurface(mazeNodeSize, (255, 255, 255))
+    gridCellSize = (mazeSize[0] / columns, mazeSize[1] / rows)
+
+    graph = Graph()
+
+    lineThick = mazeBrushThickness
+    lineColor = (255, 255, 255)
+    maxRand = 10000
+    # Generate graph and blit nodes
+    #weight = row*row+column*column
+    for row in range(0, rows):
+        for column in range(0, columns):
+            node1Pos = (mazePos[0] + column * gridCellSize[0] + gridCellSize[0] * 0.5 - mazeNodeSize[0] / 2,
+                        mazePos[1] + row * gridCellSize[1] + gridCellSize[1] * 0.5 - mazeNodeSize[1] / 2)
+            screen.blit(mazeNode, node1Pos)
+            node1 = column + row * columns
+            if (row != 0):
+                node2 = node1 - columns
+                weight = random.randrange(0,maxRand)
+                graph.add_edge(node1, node2, weight)
+                graph.add_edge(node2, node1, weight)
+            if (row != rows - 1):
+                node2 = node1 + columns
+                weight = random.randrange(0,maxRand)
+                graph.add_edge(node1, node2, weight)
+                graph.add_edge(node2, node1, weight)
+            if (column != 0):
+                node2 = node1 - 1
+                weight = random.randrange(0,maxRand)
+                graph.add_edge(node1, node2, weight)
+                graph.add_edge(node2, node1, weight)
+            if (column != columns - 1):
+                node2 = node1 + 1
+                weight = random.randrange(0,maxRand)
+                graph.add_edge(node1, node2, weight)
+                graph.add_edge(node2, node1, weight)
+
+    MSTedges = graph.createMinimumSpanningTree()
+    print("MST Length:", len(MSTedges))
+    debugPrint = False
+    delay = 0.01
+    for MSTedge in MSTedges:
+        node1 = MSTedge[0]
+        node2 = MSTedge[1]
+        node1GridPos = (node1 % columns, int(node1 / columns))
+        node2GridPos = (node2 % columns, int(node2 / columns))
+        node1Pos = (mazePos[0] + node1GridPos[0] * gridCellSize[0] + gridCellSize[0] * 0.5 - 1,
+                    mazePos[1] + node1GridPos[1] * gridCellSize[1] + gridCellSize[1] * 0.5 - 1)
+        node2Pos = (mazePos[0] + node2GridPos[0] * gridCellSize[0] + gridCellSize[0] * 0.5 - 1,
+                    mazePos[1] + node2GridPos[1] * gridCellSize[1] + gridCellSize[1] * 0.5 - 1)
+        pygame.draw.line(screen, lineColor, node1Pos, node2Pos, lineThick)
+        if (debugPrint):
+            pygame.display.update()
+            time.sleep(delay)
+    print("Generated Maze.")
+    print("Elapsed Time:",round(time.time()-startTime,5),"seconds")
+
+def quit():
+    print("Attempted Quit")
+    pygame.quit()
+    sys.exit()
+
+#Nice to have program start by calling a specific function, rather than just executing code from top
+if __name__ == '__main__':
+    main()
